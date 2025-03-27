184adc196c2dc94f3858b44a387602afddd78202
Your Name <you@example.com>
2025-03-27 19:32:59 +0000

    test after deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import Link from "next/link"
-
 export default function Home() {
   return (
     <div>
       <main>
         <h1>cloud-run-nextjs-public-env-vars-demo</h1>
-        <Link href="https://github.com/LukeSchlangen/cloud-run-nextjs-public-env-vars-demo">GitHub Repository</Link>
         <h2>Example public variable</h2>
         {process.env.NEXT_PUBLIC_EXAMPLE_VARIABLE}
       </main>
